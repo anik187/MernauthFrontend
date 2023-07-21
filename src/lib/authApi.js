befeb3c4cc0e8a2979c474e8b91212dae779b9e7
Asif Quadir <asif.anik2000@gmail.com>
2023-07-22 01:12:44 +0600

    cors

--- src/lib/authApi.js
+++ src/lib/authApi.js
@@ -1,18 +1,21 @@
 import axios from 'axios'
+//
+const baseURL = 'https://mernauthbackend-sr71.onrender.com'
+console.log(baseURL);
 async function registerUser(userData) {
-    return axios.post('/api/users', userData).then((response) => response.data)
+    return axios.post(`${baseURL}/api/users`, userData).then((response) => response.data)
 }
 
 async function loginUser(userData) {
-    return axios.post('/api/users/auth', userData).then((response) => response.data)
+    return axios.post(`${baseURL}/api/users/auth`, userData).then((response) => response.data)
 }
 
 async function logoutUser() {
-    return axios.post('/api/users/logout').then((response) => response.data);
+    return axios.post(`${baseURL}/api/users/logout`).then((response) => response.data);
 }
 
 async function updateUser(userData) {
-    return axios.put('/api/users/profile', userData).then((response) => response.data);
+    return axios.put(`${baseURL}/api/users/profile`, userData).then((response) => response.data);
 }
 
 export { registerUser, loginUser, logoutUser, updateUser }
